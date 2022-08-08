9d76ce5a064f3a6b5b67cd6d8bfd2394ae5f9d03
new-ygbot <110633406+new-ygbot@users.noreply.github.com>
2022-08-08 15:16:07 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id  =  16643256
 api_hash  =  '000d66c8f9c490c81dd64d7a48f55d1d'
-bot_token = '5402510011:AAFuRU53igSX0NSf04jq4lNCWpAiQU8n-C4'
+bot_token = '5305325879:AAFqaTKte9yCM_CEuoG8aCTcy3KUyg2SHRM'
